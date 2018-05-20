dc8fcf084e5b657a4ee226e38b8d0b1ca6c59404
David Caulton <davidcaulton@CLSLC6440DavidCaulton.local>
2018-05-20 18:42:23 -0500

    develop
    added debug_info to call in the db for troubleshooting
    We have a two step operation working now.  The first stop is a GET against a live internet website, the second is a POST against the mock server in this same application.

--- fuseplug_laravel/database/seeds/OperationsSeeder.php
+++ fuseplug_laravel/database/seeds/OperationsSeeder.php
@@ -15,6 +15,148 @@ class OperationsSeeder extends Seeder
         //
         $test_brand = DB::table('brands')->orderBy('id', 'asc')->first();
         $mock_brand = DB::table('brands')->orderBy('id', 'desc')->first();
+
+
+
+
+        // make mock endpoint for post
+        DB::table('operations')->insert([
+            'brand_id' => $mock_brand->id,
+            'name' => 'mock_post_endpoint'
+        ]);
+        $mock_post_operation = DB::table('operations')->orderBy('id', 'desc')->first();
+
+        DB::table('operation_rules')->insert([
+            'operation_id' => $mock_post_operation->id,
+            'brand_version' => 'dontcare',
+            'fuse_version' => 'dontcare',
+            'order' => 1,
+            'acting_on' => 'dontcare',
+            'do_always' => true,
+            'input_selector' => 'dontcare',
+            'operator' => '=',
+            'allowed_value' => 'dontcare'
+        ]);
+        $mock_post_operation_rule = DB::table('operation_rules')->orderBy('id', 'desc')->first();
+
+        DB::table('operation_actions')->insert([
+            'operation_rule_id' => $mock_post_operation_rule->id,
+            'order' => 1,
+            'name' => 'mock_post_operation',
+            'operation_type' => 'mock',
+            'operation_source' => 'dontcare',
+            'extra_parameters' => '{"sleep_time_min_milliseconds": "500", "sleep_time_max_milliseconds": "2000"}',
+            'http_verb' => 'POST'
+        ]);
+        $mock_post_operation_action = DB::table('operation_actions')->orderBy('id', 'desc')->first();
+
+        DB::table('data_mappings')->insert([
+            'operation_action_id' => $mock_post_operation_action->id,
+            'brand_versions' => 'dontcare',
+            'fuse_versions' => 'dontcare',
+            'object_type_being_created' => 'payload',
+            'template' => '{"caller": "{from}", "processed_at": "{current_datetime}", "test_url": "{test_root_url}"}'
+        ]);
+        $data_mapping = DB::table('data_mappings')->orderBy('id', 'desc')->first();
+
+        DB::table('data_mapping_details')->insert([
+            'data_mapping_id' => $data_mapping->id,
+            'order' => 1,
+            'source_field' => 'subtitle',
+            'source_field_type' => 'payload',
+            'target_field' => 'from',
+            'target_data_type' => 'payload',
+            'default_value' => 'some guy'
+        ]);
+        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
+
+        DB::table('data_mapping_details')->insert([
+            'data_mapping_id' => $data_mapping->id,
+            'order' => 2,
+            'source_field' => '',
+            'source_field_type' => '',
+            'target_field' => 'current_datetime',
+            'target_data_type' => 'payload',
+            'target_format_string' => 'M d, Y D H:m:s',
+            'transform' => 'php_format_date',
+            'default_value' => ''
+        ]);
+        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
+        DB::table('data_mapping_details')->insert([
+            'data_mapping_id' => $data_mapping->id,
+            'order' => 3,
+            'source_field' => 'TEST_CLIENT_ROOT_URL',
+            'source_field_type' => '',
+            'target_field' => 'test_root_url',
+            'target_data_type' => 'payload',
+            'transform' => 'env_variable',
+            'default_value' => 'http://im_a_luzer.com'
+        ]);
+        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
+
+
+        // make mock endpoint for get
+        DB::table('operations')->insert([
+            'brand_id' => $mock_brand->id,
+            'name' => 'mock_get_endpoint'
+        ]);
+        $mock_get_operation = DB::table('operations')->orderBy('id', 'desc')->first();
+
+        DB::table('operation_rules')->insert([
+            'operation_id' => $mock_get_operation->id,
+            'brand_version' => 'dontcare',
+            'fuse_version' => 'dontcare',
+            'order' => 1,
+            'acting_on' => 'dontcare',
+            'do_always' => true,
+            'input_selector' => 'dontcare',
+            'operator' => '=',
+            'allowed_value' => 'dontcare'
+        ]);
+        $mock_get_operation_rule = DB::table('operation_rules')->orderBy('id', 'desc')->first();
+
+        DB::table('operation_actions')->insert([
+            'operation_rule_id' => $mock_get_operation_rule->id,
+            'order' => 1,
+            'name' => 'mock_get_operation',
+            'operation_type' => 'mock',
+            'operation_source' => 'dontcare',
+            'extra_parameters' => '{"sleep_time_min_milliseconds": "250", "sleep_time_max_milliseconds": "500"}',
+            'http_verb' => 'GET'
+        ]);
+        $mock_get_operation_action = DB::table('operation_actions')->orderBy('id', 'desc')->first();
+
+        DB::table('data_mappings')->insert([
+            'operation_action_id' => $mock_get_operation_action->id,
+            'brand_versions' => 'dontcare',
+            'fuse_versions' => 'dontcare',
+            'object_type_being_created' => 'payload',
+            'template' => '{"processed_at": "{current_datetime}"}'
+        ]);
+        $data_mapping = DB::table('data_mappings')->orderBy('id', 'desc')->first();
+
+        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
+        DB::table('data_mapping_details')->insert([
+            'data_mapping_id' => $data_mapping->id,
+            'order' => 2,
+            'source_field' => '',
+            'source_field_type' => '',
+            'target_field' => 'current_datetime',
+            'target_data_type' => 'payload',
+            'target_format_string' => 'M d, Y D H:m:s',
+            'transform' => 'php_format_date',
+            'default_value' => ''
+        ]);
+        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
+
+
+
+
+
+
+
+
+        // live endpoint with one live get and a post against our mock endpoint
         DB::table('operations')->insert([
             'brand_id' => $test_brand->id,
             'name' => 'credit_check_laravel',
@@ -38,7 +180,7 @@ class OperationsSeeder extends Seeder
         DB::table('operation_actions')->insert([
             'operation_rule_id' => $operation_rule->id,
             'order' => 1,
-            'name' => 'hit foaas company endpoint',
+            'name' => 'GET to foaas',
             'operation_type' => 'http',
             'operation_source' => 'fuse',
             'http_verb' => 'GET'
@@ -49,6 +191,7 @@ class OperationsSeeder extends Seeder
             'operation_action_id' => $operation_action->id,
             'brand_versions' => 'v1,v2',
             'template' => '{brand_root_url}/anyway/{company}/{from}',
+            'object_type_being_created' => 'url',
             'fuse_versions' => '57-59'
         ]);
         $data_mapping = DB::table('data_mappings')->orderBy('id', 'desc')->first();
@@ -86,6 +229,55 @@ class OperationsSeeder extends Seeder
             'default_value' => 'http://it_didnt_get_set.com'
         ]);
         $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
+
+        DB::table('operation_actions')->insert([
+            'operation_rule_id' => $operation_rule->id,
+            'order' => 2,
+            'name' => 'POST to mock endpoint',
+            'operation_type' => 'http',
+            'operation_source' => 'fuse',
+            'http_verb' => 'POST'
+        ]);
+        $operation_action = DB::table('operation_actions')->orderBy('id', 'desc')->first();
+
+        DB::table('data_mappings')->insert([
+            'operation_action_id' => $operation_action->id,
+            'brand_versions' => 'v1,v2',
+            'template' => 'http://localhost:8000/mock/' . $mock_post_operation->id,
+            'object_type_being_created' => 'url',
+            'fuse_versions' => '57-59'
+        ]);
+        $data_mapping = DB::table('data_mappings')->orderBy('id', 'desc')->first();
+
+        DB::table('data_mappings')->insert([
+            'operation_action_id' => $operation_action->id,
+            'brand_versions' => 'v1,v2',
+            'template' => '{"subtitle": "{subtitle}"}',
+            'object_type_being_created' => 'payload',
+            'fuse_versions' => '57-59'
+        ]);
+        $data_mapping = DB::table('data_mappings')->orderBy('id', 'desc')->first();
+
+        DB::table('data_mapping_details')->insert([
+            'data_mapping_id' => $data_mapping->id,
+            'order' => 1,
+            'source_field' => 'subtitle',
+            'source_field_type' => 'payload',
+            'target_field' => 'subtitle',
+            'target_data_type' => 'payload',
+            'default_value' => 'many people speaking at once'
+        ]);
+        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
+
+
+
+
+
+
+
+
+
+
 
         DB::table('operations')->insert([
             'brand_id' => $test_brand->id,
@@ -135,77 +327,12 @@ class OperationsSeeder extends Seeder
         ]);
         $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
 
-        // make mock endpoint for post
-        DB::table('operations')->insert([
-            'brand_id' => $mock_brand->id,
-            'name' => 'mock_post_endpoint'
-        ]);
-        $operation = DB::table('operations')->orderBy('id', 'desc')->first();
 
-        DB::table('operation_rules')->insert([
-            'operation_id' => $operation->id,
-            'brand_version' => 'dontcare',
-            'fuse_version' => 'dontcare',
-            'order' => 1,
-            'acting_on' => 'dontcare',
-            'do_always' => true,
-            'input_selector' => 'dontcare',
-            'operator' => '=',
-            'allowed_value' => 'dontcare'
-        ]);
-        $operation_rule = DB::table('operation_rules')->orderBy('id', 'desc')->first();
 
-        DB::table('operation_actions')->insert([
-            'operation_rule_id' => $operation_rule->id,
-            'order' => 1,
-            'name' => 'mock_post_operation',
-            'operation_type' => 'mock',
-            'operation_source' => 'dontcare',
-            'extra_parameters' => '{"sleep_time_min_milliseconds": "500", "sleep_time_max_milliseconds": "2000"}',
-            'http_verb' => 'POST'
-        ]);
-        $operation_action = DB::table('operation_actions')->orderBy('id', 'desc')->first();
 
-        DB::table('data_mappings')->insert([
-            'operation_action_id' => $operation_action->id,
-            'brand_versions' => 'dontcare',
-            'fuse_versions' => 'dontcare',
-            'template' => '{"this_is_from": "{from}", "processed_at": "{current_datetime}", "test_url": "{test_root_url}"}'
-        ]);
-        $data_mapping = DB::table('data_mappings')->orderBy('id', 'desc')->first();
 
-        DB::table('data_mapping_details')->insert([
-            'data_mapping_id' => $data_mapping->id,
-            'order' => 1,
-            'source_field' => 'from',
-            'source_field_type' => 'payload',
-            'target_field' => 'from',
-            'target_data_type' => 'payload',
-            'default_value' => 'some guy'
-        ]);
-        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
-        DB::table('data_mapping_details')->insert([
-            'data_mapping_id' => $data_mapping->id,
-            'order' => 2,
-            'source_field' => '',
-            'source_field_type' => '',
-            'target_field' => 'current_datetime',
-            'target_data_type' => 'payload',
-            'target_format_string' => 'M d, Y D H:m:s',
-            'transform' => 'php_format_date',
-            'default_value' => ''
-        ]);
-        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
-        DB::table('data_mapping_details')->insert([
-            'data_mapping_id' => $data_mapping->id,
-            'order' => 3,
-            'source_field' => 'TEST_CLIENT_ROOT_URL',
-            'source_field_type' => '',
-            'target_field' => 'test_root_url',
-            'target_data_type' => 'payload',
-            'transform' => 'env_variable',
-            'default_value' => 'http://im_a_luzer.com'
-        ]);
-        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
+
+
+
     }
 }
