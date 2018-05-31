2f5e8b45dafab739eb4fd1c3aae2f8bface335fb
David Caulton <davidcaulton@CLSLC6440DavidCaulton.local>
2018-05-31 15:33:48 -0500

    develop
    got the python working to consume multiple messages again

--- fuseplug_laravel/database/seeds/OperationsSeeder.php
+++ fuseplug_laravel/database/seeds/OperationsSeeder.php
@@ -48,7 +48,7 @@ class OperationsSeeder extends Seeder
             'name' => 'mock_post_echo_operation_action',
             'operation_type' => 'mock',
             'operation_source' => 'dontcare',
-            'extra_parameters' => '{"sleep_time_min_milliseconds": "500", "sleep_time_max_milliseconds": "2000"}',
+            'extra_parameters' => '{"sleep_time_min_milliseconds": "100", "sleep_time_max_milliseconds": "100"}',
             'http_verb' => 'POST'
         ]);
         $mock_post_operation_action = DB::table('operation_actions')->orderBy('id', 'desc')->first();
@@ -89,7 +89,7 @@ class OperationsSeeder extends Seeder
             'name' => 'mock_post_operation',
             'operation_type' => 'mock',
             'operation_source' => 'dontcare',
-            'extra_parameters' => '{"sleep_time_min_milliseconds": "500", "sleep_time_max_milliseconds": "2000"}',
+            'extra_parameters' => '{"sleep_time_min_milliseconds": "100", "sleep_time_max_milliseconds": "100"}',
             'http_verb' => 'POST'
         ]);
         $mock_post_operation_action = DB::table('operation_actions')->orderBy('id', 'desc')->first();
@@ -165,7 +165,7 @@ class OperationsSeeder extends Seeder
             'name' => 'mock_get_operation',
             'operation_type' => 'mock',
             'operation_source' => 'dontcare',
-            'extra_parameters' => '{"sleep_time_min_milliseconds": "250", "sleep_time_max_milliseconds": "500"}',
+            'extra_parameters' => '{"sleep_time_min_milliseconds": "100", "sleep_time_max_milliseconds": "100"}',
             'http_verb' => 'GET'
         ]);
         $mock_get_operation_action = DB::table('operation_actions')->orderBy('id', 'desc')->first();
@@ -175,14 +175,14 @@ class OperationsSeeder extends Seeder
             'brand_versions' => 'dontcare',
             'fuse_versions' => 'dontcare',
             'object_type_being_created' => 'payload',
-            'template' => '{"processed_at": "{current_datetime}"}'
+            'template' => '{"processed_at": "{current_datetime}", "subtitle": "{subtitle}"}'
         ]);
         $data_mapping = DB::table('data_mappings')->orderBy('id', 'desc')->first();
 
         $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
         DB::table('data_mapping_details')->insert([
             'data_mapping_id' => $data_mapping->id,
-            'order' => 2,
+            'order' => 1,
             'source_field' => '',
             'source_field_type' => '',
             'target_field' => 'current_datetime',
@@ -193,6 +193,16 @@ class OperationsSeeder extends Seeder
         ]);
         $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
 
+        DB::table('data_mapping_details')->insert([
+            'data_mapping_id' => $data_mapping->id,
+            'order' => 2,
+            'source_field' => 'subtitle',
+            'source_field_type' => 'payload',
+            'target_field' => 'subtitle',
+            'target_data_type' => 'payload',
+            'default_value' => 'some guy'
+        ]);
+        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
 
 
 
@@ -224,7 +234,7 @@ class OperationsSeeder extends Seeder
         DB::table('operation_actions')->insert([
             'operation_rule_id' => $operation_rule->id,
             'order' => 1,
-            'name' => 'GET to foaas',
+            'name' => 'GET to mock get endpoint',
             'operation_type' => 'http',
             'operation_source' => 'fuse',
             'http_verb' => 'GET'
@@ -234,8 +244,17 @@ class OperationsSeeder extends Seeder
         DB::table('data_mappings')->insert([
             'operation_action_id' => $operation_action->id,
             'brand_versions' => 'v1,v2',
-            'template' => '{brand_root_url}/anyway/{company}/{from}',
+            'template' => 'http://localhost:8000/mock/' . $mock_get_operation->id,
             'object_type_being_created' => 'url',
+            'fuse_versions' => '57-59'
+        ]);
+        $data_mapping = DB::table('data_mappings')->orderBy('id', 'desc')->first();
+
+        DB::table('data_mappings')->insert([
+            'operation_action_id' => $operation_action->id,
+            'brand_versions' => 'v1,v2',
+            'template' => '{"subtitle": "{subtitle}"}',
+            'object_type_being_created' => 'payload',
             'fuse_versions' => '57-59'
         ]);
         $data_mapping = DB::table('data_mappings')->orderBy('id', 'desc')->first();
@@ -245,32 +264,9 @@ class OperationsSeeder extends Seeder
             'order' => 1,
             'source_field' => 'from',
             'source_field_type' => 'payload',
-            'target_field' => 'from',
-            'target_data_type' => 'url',
-            'default_value' => 'somebody important'
-        ]);
-        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
-
-        DB::table('data_mapping_details')->insert([
-            'data_mapping_id' => $data_mapping->id,
-            'order' => 2,
-            'source_field' => 'company',
-            'source_field_type' => 'url',
-            'target_field' => 'company',
-            'target_data_type' => 'url',
-            'skip_if_empty' => true,
-            'default_value' => 'Honda of America'
-        ]);
-        $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
-
-        DB::table('data_mapping_details')->insert([
-            'data_mapping_id' => $data_mapping->id,
-            'order' => 3,
-            'source_field' => 'TEST_CLIENT_ROOT_URL',
-            'target_field' => 'brand_root_url',
-            'target_data_type' => 'url',
-            'transform' => 'env_variable',
-            'default_value' => 'http://it_didnt_get_set.com'
+            'target_field' => 'subtitle',
+            'target_data_type' => 'payload',
+            'default_value' => 'idi amin'
         ]);
         $data_mapping_detail = DB::table('data_mapping_details')->orderBy('id', 'desc')->first();
 
